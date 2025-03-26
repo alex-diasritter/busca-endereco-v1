04f2ecba6cb27b9fc9968062972861c35e92a829
Lenovo <ritter.alex@hotmail.com>
2025-03-26 16:15:19 -0300

    add: AuthorizationController + Swagger

--- src/main/java/com/alex/buscacep/BuscacepApplication.java
+++ src/main/java/com/alex/buscacep/BuscacepApplication.java
@@ -1,8 +1,19 @@
 package com.alex.buscacep;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "API BuscaEndereco",
+				version = "1.0",
+				description = "Faça login ou registre-se para acessar qualquer endereço do Brasil.",
+				contact = @Contact(name = "Alex Ritter", email = "ritter.alex@hotmail.com")
+		)
+)
 @SpringBootApplication
 public class BuscacepApplication {
 
--- src/main/java/com/alex/buscacep/controller/AuthenticationController.java
+++ src/main/java/com/alex/buscacep/controller/AuthenticationController.java
@@ -7,6 +7,11 @@ import com.alex.buscacep.domain.dtos.request.RegisterRequestDTO;
 import com.alex.buscacep.domain.dtos.response.LoginResponseDTO;
 import com.alex.buscacep.infra.security.TokenService;
 import com.alex.buscacep.infra.service.AuthorizationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -29,6 +34,28 @@ public class AuthenticationController {
     @Autowired
     private AuthorizationService authorizationService;
 
+    @Operation(summary = "Rota responsável pelo login de usuários")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Usuário logado com sucesso",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Usuário não encontrado",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @PostMapping("/login")
     public ResponseEntity<LoginResponseDTO> login(@RequestBody @Valid AuthenticationRequestDTO data){
         var login = new UsernamePasswordAuthenticationToken(data.username(), data.password());
@@ -37,20 +64,54 @@ public class AuthenticationController {
         return ResponseEntity.ok(new LoginResponseDTO(token));
     }
 
+    @Operation(summary = "Rota responsável pelo cadastro de usuários")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Usuário cadastrado com sucesso",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    }
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Informação inválida",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json"
+                            )
+                    }
+            )
+    })
     @PostMapping("/register")
     public ResponseEntity<UserDTO> register(@RequestBody @Valid RegisterRequestDTO data){
         var user = authorizationService.register(data);
         return ResponseEntity.ok(user);
     }
 
+
+    @Operation(summary = "Rota responsável por listar usuários do sistema")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Lista de usuários retornada",
+                    content = {
+                            @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = User.class)
+                            )
+                    }
+            )})
     @GetMapping("/users")
     public ResponseEntity<Page<UserDTO>> list(Pageable pageable){
         return ResponseEntity.ok(authorizationService.findAll(pageable));
     }
 
 
-    //Está retornando 403 e
-
+    //Está retornando 403
     @DeleteMapping("/{username}")
     public ResponseEntity delete(@PathVariable String username){
         var result = authorizationService.delete(username);
