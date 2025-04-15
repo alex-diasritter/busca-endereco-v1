8781effdf2c55542dc608500e469af31c56d1b64
Lenovo <ritter.alex@hotmail.com>
2025-04-15 12:42:03 -0300

    add: swagger authorization

--- src/main/java/com/alex/buscacep/BuscacepApplication.java
+++ src/main/java/com/alex/buscacep/BuscacepApplication.java
@@ -1,8 +1,12 @@
 package com.alex.buscacep;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,7 +16,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 				version = "1.0",
 				description = "Faça login ou registre-se para acessar qualquer endereço do Brasil.",
 				contact = @Contact(name = "Alex Ritter", email = "ritter.alex@hotmail.com")
-		)
+		),
+		security = @SecurityRequirement(name = "bearerAuth") // Adicione esta linha
+)
+@SecurityScheme(
+		name = "bearerAuth",
+		type = SecuritySchemeType.HTTP,
+		scheme = "bearer",
+		bearerFormat = "JWT", // opcional, apenas para informar o formato do token
+		in = SecuritySchemeIn.HEADER
 )
 @SpringBootApplication
 public class BuscacepApplication {
